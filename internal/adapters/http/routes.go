0790a2a787d1a2126e9cdabee2be16ff8e06e66d
merfawaladam <mrefawaladam@gmail.com>
2023-05-03 20:44:31 +0700

    fix middleware

--- internal/adapters/http/routes.go
+++ internal/adapters/http/routes.go
@@ -6,10 +6,11 @@ import (
 	middlewares "ebook/internal/adapters/http/middleware"
 	repository "ebook/internal/adapters/repository"
 	usecase "ebook/internal/application/usecase"
-	"fmt"
 
+	"fmt"
 	"net/http"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -40,10 +41,13 @@ func InitRoutes() *echo.Echo {
 	// middleware
 	e.Use(middleware.Logger())
 	// protected route
+
 	protected := e.Group("/protected")
 	protected.Use(middlewares.AuthMiddleware())
 	protected.GET("", func(c echo.Context) error {
-		userID := c.Get("userID").(int)
+		user := c.Get("user").(*jwt.Token)
+		claims := user.Claims.(*jwt.MapClaims)
+		userID := int((*claims)["id"].(float64))
 		return c.String(http.StatusOK, fmt.Sprintf("User ID: %d", userID))
 	})
 
