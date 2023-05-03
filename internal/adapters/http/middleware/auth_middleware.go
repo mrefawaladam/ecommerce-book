7a0412d720684e5d33b981c87016aa9459906c75
merfawaladam <mrefawaladam@gmail.com>
2023-05-03 18:35:32 +0700

    add middleware protected route

--- internal/adapters/http/middleware/auth_middleware.go
+++ internal/adapters/http/middleware/auth_middleware.go
@@ -8,6 +8,8 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+const SECRET_JWT = "123"
+
 func CreateToken(userID int, email string, role string) (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
 
@@ -17,7 +19,7 @@ func CreateToken(userID int, email string, role string) (string, error) {
 	claims["role"] = role
 	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 
-	tokenString, err := token.SignedString([]byte("secret-key"))
+	tokenString, err := token.SignedString([]byte(SECRET_JWT))
 	if err != nil {
 		return "", err
 	}
@@ -34,6 +36,42 @@ func RequireRole(role string) echo.MiddlewareFunc {
 			if userRole != role {
 				return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Unauthorized access"})
 			}
+
+			return next(c)
+		}
+	}
+}
+
+func AuthMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			tokenString := c.Request().Header.Get("Authorization")
+			if tokenString == "" {
+				return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Token is missing"})
+			}
+
+			token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, echo.NewHTTPError(http.StatusBadRequest, "Invalid token")
+				}
+				return []byte(SECRET_JWT), nil
+			})
+
+			if err != nil {
+				return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Invalid token"})
+			}
+
+			claims, ok := token.Claims.(jwt.MapClaims)
+			if !ok || !token.Valid {
+				return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Invalid token"})
+			}
+
+			userID, ok := claims["id"].(float64)
+			if !ok {
+				return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Invalid token"})
+			}
+
+			c.Set("userID", int(userID))
 
 			return next(c)
 		}
--- internal/adapters/http/routes.go
+++ internal/adapters/http/routes.go
@@ -3,23 +3,30 @@ package http
 import (
 	db "ebook/internal/adapters/db/mysql"
 	handler "ebook/internal/adapters/http/handler"
+	middlewares "ebook/internal/adapters/http/middleware"
 	repository "ebook/internal/adapters/repository"
 	usecase "ebook/internal/application/usecase"
+	"fmt"
+
+	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 var (
 	userRepo    repository.Repository
 	userHandler handler.HandlerUser
 	userUsecase usecase.Usecase
+
+	AuthHandler handler.AuthHandler
 )
 
 func declare() {
 	userRepo = repository.Repository{DB: db.DbMysql}
 	userUsecase = usecase.Usecase{Repo: userRepo}
 	userHandler = handler.HandlerUser{Usecase: userUsecase}
-
+	AuthHandler = handler.AuthHandler{Usecase: userUsecase}
 }
 
 func InitRoutes() *echo.Echo {
@@ -27,6 +34,19 @@ func InitRoutes() *echo.Echo {
 	declare()
 
 	e := echo.New()
+	e.POST("/login", AuthHandler.Login())
+	e.POST("/registrasi", AuthHandler.Register())
+
+	// middleware
+	e.Use(middleware.Logger())
+	// protected route
+	protected := e.Group("/protected")
+	protected.Use(middlewares.AuthMiddleware())
+	protected.GET("", func(c echo.Context) error {
+		userID := c.Get("userID").(int)
+		return c.String(http.StatusOK, fmt.Sprintf("User ID: %d", userID))
+	})
+
 	user := e.Group("/users")
 	user.GET("", userHandler.GetAllUsers())
 	user.GET("/:id", userHandler.GetUser())
