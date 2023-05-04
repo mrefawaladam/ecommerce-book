2548e8e2993d31e1b1d52c1e7cfe9494bbb628ea
mrefawaladam <aerefa55@gmail.com>
2023-05-04 22:58:53 +0700

    create DecreaseStock and IncreaseStock in entity book

--- internal/entity/book.go
+++ internal/entity/book.go
@@ -1,6 +1,8 @@
 package entity
 
 import (
+	"errors"
+
 	"gorm.io/gorm"
 )
 
@@ -9,13 +11,25 @@ type Book struct {
 
 	Title       string   `json:"title" form:"title validate:"required"`
 	Author      string   `json:"author" form:"author validate:"required"`
-	Cover       string   `json:"cover" form:"cover validate:"required" `
-	Status      string   `json:"status" form:"status" validate:"required" `
+	Cover       string   `json:"cover" form:"cover validate:"required"`
+	Status      string   `json:"status" form:"status" validate:"required"`
 	Description string   `json:"description" form:"description" validate:"required"`
-	Stok        string   `json:"stok" form:"stok" validate:"required"`
-	Price       string   `json:"price" form:"price" validate:"required"`
-	Weight      string   `json:"weight" form:"weight" validate:"required"`
+	Stok        uint     `json:"stok" form:"stok" validate:"required"`
+	Price       float64  `json:"price" form:"price" validate:"required"`
+	Weight      float64  `json:"weight" form:"weight" validate:"required"`
 	CategoryID  uint     `json:"category_id" form:"category_id" validate:"required"`
 	SellerId    uint     `json:"seller_id" form:"seller_id"`
 	Category    Category `json:"category" gorm:"foreignKey:CategoryID"`
+}
+
+func (b *Book) DecreaseStock(quantity uint) error {
+	if b.Stok < quantity {
+		return errors.New("not enough stock")
+	}
+	b.Stok -= quantity
+	return nil
+}
+
+func (b *Book) IncreaseStock(quantity uint) {
+	b.Stok += quantity
 }
